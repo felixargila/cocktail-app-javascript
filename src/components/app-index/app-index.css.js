1511771856bc3c59f921bc022ca0c545fee4c1ad
Felix Argila <felix.argila@gmail.com>
2025-08-05 21:09:28 +0200

    general header added

--- src/components/app-index/app-index.css.js
+++ src/components/app-index/app-index.css.js
@@ -23,5 +23,78 @@ export default css`
     height: 100%;
     box-sizing: border-box;
   }
+
+  main {
+    flex: 1;
+    position: relative;
+    overflow: hidden;
+  }
+
+  main ::slotted(*) {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    visibility: hidden;
+  }
+
+  main ::slotted([state="active"]) {
+    visibility: visible;
+  }
+
+  header {
+    width: 100%;
+    background-color: var(--surface);
+    color: var(--primary);
+    padding: 0px 1rem;
+    box-sizing: border-box;
+  }
+
+  .header-content {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    position: relative;
+    padding: 1rem 0px;
+  }
  
+  .header-logo {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  h1 {
+    font-size: 1.5rem;
+    line-height: 1.5rem;
+    font-weight: 300;
+    color: var(--primary);
+  }
+
+  a {
+    text-decoration: none;
+    color: var(--primary);
+  }
+
+  /* 1024px */
+  @media (min-width: 64rem) {
+    header {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      max-width: 58rem;
+      height: 8rem;
+      margin: 0 auto;
+      will-change: height;
+      transition: height 0.1s ease-in-out;
+    }
+  }
+
+  /* 1440px */
+  @media (min-width: 90rem) {
+    header {
+      max-width: 71.5rem;
+    }
+  }
 `;
--- src/components/app-index/app-index.js
+++ src/components/app-index/app-index.js
@@ -4,6 +4,8 @@ import { ElementController } from '@open-cells/element-controller';
 import { routes } from '../../router/routes.js';
 import styles from './app-index.css.js';
 import { appConfig } from '../../config/app.config.js';
+import '@open-cells/page-transitions/page-transition-head-styles.js';
+import '@material/web/icon/icon.js';
 
 startApp({
   routes,
@@ -26,25 +28,50 @@ export class AppIndex extends LitElement {
   constructor() {
     super();
     this.elementController = new ElementController(this);
+    this._header = null;
     this._root = null;
   }
 
   connectedCallback() {
     super.connectedCallback();
+    this.elementController.subscribe('scroll', (data) => this._headerTransition(data));
   }
 
   firstUpdated(props) {
     super.firstUpdated(props);
 
+    this._header = this.shadowRoot?.querySelector('header');
     this._root = document.querySelector(':root');
   }
 
   render() {
     return html`
+      ${this._headerTpl}
       <main role="main" tabindex="-1">
         <slot></slot>
       </main>
     `;
+  }
+
+  get _headerTpl() {
+    return html`
+      <header>
+        <div class="header-content">
+          <div class="header-logo">
+            <md-icon>local_bar</md-icon>
+            <h1><a href="#!/">Cells Cocktails</a></h1>
+          </div>
+        </div>
+      </header>
+    `;
+  }
+
+  _headerTransition(data) {
+    if (data.scrollTop > 0) {
+      this._header?.classList.add('scrolled');
+    } else {
+      this._header?.classList.remove('scrolled');
+    }
   }
 }
 
